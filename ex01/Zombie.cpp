b4b4de2d7efcac81e01a51c3c9ec313f81c1e87a
Razmik Kocharyan <rkochary@c2r16s2.42yerevan.am>
2023-05-08 18:44:02 +0400

    initial

---
+++ ex01/Zombie.cpp
@@ -0,0 +1,31 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie()
+{
+    // std::cout << name << ":" << "is created"<<std::endl;
+}
+
+Zombie::~Zombie()
+{
+
+}
+
+void Zombie::announce()
+{
+    std::cout << name << ": " << "BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+void Zombie::setName(std::string str)
+{
+    name = str;
+}
+
+Zombie* zombieHorde(int N,std::string name)
+{
+    Zombie* a = new Zombie[N];
+    for(int i = 0;i < N;i++)
+    {
+        a[i].setName(name);
+    }
+    return a;
+}
---
+++ ex01/main.cpp
@@ -0,0 +1,20 @@
+#include "Zombie.hpp"
+
+int main(int ac,char **av)
+{
+
+    if(ac != 3)
+    {
+        std::cout << "Wrong parameters,please try again!" << std::endl;
+    }
+    else
+    {
+
+        Zombie *z = zombieHorde(atoi(av[1]),av[2]);
+        for(int i = 0; i < atoi(av[1]);i++)
+        {
+            z[i].announce();
+        }
+        delete[] z;
+    }
+}
